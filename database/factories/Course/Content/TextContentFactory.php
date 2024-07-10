25c0094b18db34d820b36f4a63e13bc3705952f9
Fabio Baptista <24209013+fabiobap@users.noreply.github.com>
2024-07-09 23:57:31 -0300

    factories wip

---
+++ database/factories/Course/Content/TextContentFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories\Course\Content;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Course\Content\TextContent>
+ */
+class TextContentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
---
+++ database/factories/Course/Quiz/QuestionFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories\Course\Quiz;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Course\Quiz\Question>
+ */
+class QuestionFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+        ];
+    }
+}
